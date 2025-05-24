82bb8f5ee7d442c37c45987e9e672285cc30c4c0
DanilMikhailv <Danil_Mikhailov_5@mail.ru>
2025-05-24 14:17:35 +0300

    Добавил возможность вписать имя пользователя

---
+++ projects/hello-world.cpp/hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int maint()
+{
+	std::string name;
+	std::cout<<"ВВедите свое имя";
+	std::cin>>name;
+	std::cout<<"Hello world from:"<<name<<std::endl;
+	return 0;
+}
+
