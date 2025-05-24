ff5d469520863cf9a8bfc17c669ba208f954a13f
DanilMikhailv <Danil_Mikhailov_5@mail.ru>
2025-05-24 14:35:22 +0300

    Убрал комментарии

--- projects/hello-world.cpp/hello_world.cpp
+++ projects/hello-world.cpp/hello_world.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
-//Основная функция
+
 int maint()
 {
-	std::string name;//Переменная которая хранит имя пользователя
-	std::cout<<"ВВедите свое имя";//Запрос имени
-	std::cin>>name;//Ввод имени пользователя
-	std::cout<<"Hello world from:"<<name<<std::endl;//Вывод сообщения
+	std::string name;
+	std::cout<<"ВВедите свое имя";
+	std::cin>>name;
+	std::cout<<"Hello world from:"<<name<<std::endl;
 	return 0;
 }
 
