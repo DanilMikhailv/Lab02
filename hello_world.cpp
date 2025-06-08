41f3a7f34b2b122087ffdcb3f625a3c332482422
DanilMikhailv <danil_mikhailov_5@mail.ru>
2025-06-08 03:08:50 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,14 +2,15 @@
 #include <string>
 
 // Main function that greets the user
-int main()
+int
+main()
 {
-    std::string name; // Variable to store user's name
+  std::string name; // Variable to store user's name
 
-    std::cin >> name; // Prompt user for input
+  std::cin >> name; // Prompt user for input
 
-    // Output greeting message
-    std::cout << "Hello World from " << name << std::endl;
+  // Output greeting message
+  std::cout << "Hello World from " << name << std::endl;
 
-    return 0;
+  return 0;
 }
